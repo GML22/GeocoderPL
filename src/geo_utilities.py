e98957f805aa8025829f4a61305e9ed4c0d94b6b
GML22 <lodgar.gomul@gmail.com>
2021-11-29 13:01:44 +0100

    Final commit

--- src/geo_utilities.py
+++ src/geo_utilities.py
@@ -12,8 +12,6 @@ from pyproj import Proj, transform
 from shapely.geometry import shape, mapping
 from shapely.ops import cascaded_union
 
-import ogr2ogr
-
 
 def time_decorator(func):
     """ Decorator that prints information about time of function execution """
@@ -74,91 +72,6 @@ def clear_xml_node(curr_node):
     for ancestor in curr_node.xpath('ancestor-or-self::*'):
         while ancestor.getprevious() is not None:
             del ancestor.getparent()[0]
-
-
-def raster2geojson():
-    """ Function that generalize and vectorise raster GeoTiff file and finally converts it to geojson format """
-
-    base_dir = os.getcwd()
-    driver = gdal.GetDriverByName('GTiff')
-    in_ds = gdal.Open(base_dir + "\\temp1.tif", gdal.GA_ReadOnly)
-    myarray = in_ds.GetRasterBand(1).ReadAsArray()
-    myarray[myarray < 0] = 0
-
-    if myarray is None or in_ds is None:
-        print("myarray or in_ds is None for:" + base_dir + "\\temp1.tif")
-
-    # Tworzymy pomocniczy obiekt GeoTiff, przypisujemy mu poprawioną macierz i zapisujemy go na dysku
-    r = driver.Create(base_dir + "\\temp1.tif", in_ds.RasterXSize, in_ds.RasterYSize, 1, gdal.GDT_Int16,
-                      options=['COMPRESS=LZW'])
-    r.SetGeoTransform(in_ds.GetGeoTransform())
-    r.SetProjection(in_ds.GetProjection())
-    r.GetRasterBand(1).WriteArray(myarray)
-    # in_ds = None
-    # r = None
-
-    # Czekamy 1 sekundę, żeby plik zdążył się zapisać
-    time.sleep(1)
-
-    # Otwieramy nowoutworzony plik temp1.tif
-    in_ds2 = gdal.Open(base_dir + "\\temp1.tif", gdal.GA_ReadOnly)
-
-    # Generalizujemy nowoutworzony plik do postaci 50m x 50m przy użyciu algorytmu uśredniającego
-    raster = gdal.Wrap(base_dir + "\\temp1.tif", in_ds2, format='GTiff', xRes=50, yRes=50,
-                       resampleAlg=gdal.GRA_CubicSpline, options=['COMPRESS=LZW'])
-    # in_ds2 = None
-
-    myarray2 = raster.GetRasterBand(1).ReadAsArray().astype(float)
-    myarray2[myarray2 > 0] = 1
-
-    new_shp_fn = base_dir + "\\test.shp"
-    shp_driver = ogr.GetDriverByName('ESRI Shapefile')
-    srs = osr.SpatialReference()
-    srs.ImportFromWkt(raster.GetProjectionRef())
-    out_data_source = shp_driver.CreateDataSource(new_shp_fn)
-    out_layer = out_data_source.CreateLayer(new_shp_fn, srs=srs)
-    new_field = ogr.FieldDefn('Depth', ogr.OFTInteger)
-    out_layer.CreateField(new_field)
-    band = raster.GetRasterBand(1)
-    band.WriteArray(myarray2)
-
-    # Wektoryzujemy warstwę
-    gdal.Polygonize(band, None, out_layer, 0, [], callback=None)
-    out_data_source.Destroy()
-    # raster = None
-    os.remove(base_dir + "\\temp1.tif")
-    os.remove(base_dir + "\\temp0.tif")
-
-    # Czekamy 1 sekundę, żeby plik zdążył się zapisać
-    time.sleep(1)
-
-    # Laczenie wszystkich shapefilow w jeden laczny plik shp
-    os.chdir(base_dir + "\\test")
-    shp_list = glob.glob("*.shp")
-    meta = fiona.open(shp_list[0]).meta
-
-    with fiona.open("merged_file.shp", 'w', **meta) as output:
-        geom_list = [
-            [shape(features['geometry']) for features in fiona.open(shp) if features['properties']['Depth'] == 1]
-            for shp in shp_list]
-        polygons = []
-
-        for lst1 in geom_list:
-            polygons += lst1
-
-        union = cascaded_union(polygons)
-        output.write({'geometry': mapping(union), 'properties': {'Depth': 1}})
-
-    # Upraszczamy shapefile, żeby zmniejszyć jego wagę
-    ogr2ogr.main(["", "-f", "ESRI Shapefile", 'merged_file_ligth.shp', 'merged_file.shp', '-simplify', "50", "-lco",
-                  "ENCODING=Windows-1250"])
-
-    # Zapisujemy uzyskany plik jako GEOJSON
-    # Opcja "RFC7946=YES" zostala dodana, żeby generował się plik geojson we właściwym formacie
-    # Opcja "COORDINATE_PRECISION=5" zostala dodana, żeby koordynaty miały precyzję pięciu miejsc po przecinku, czyli
-    # mniej więcej 1 metra
-    ogr2ogr.main(["", "-f", "GeoJSON", "test.geojson", "merged_file_light.shp", "-lco", "RFC7946=YES", "-lco",
-                  "COORDINATE_PRECISION=4"])
 
 
 @lru_cache
